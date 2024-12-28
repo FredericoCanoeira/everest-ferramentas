eeed32277d98110357969c7876b1be466ed94ede
Frederico Canoeira <fcanoeira@gmail.com>
2024-12-28 16:32:16 -0500

    Ajustar rotas para integrar novas páginas Home e SelfAssessment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,15 +4,21 @@ import Login from "./components/Login";
 import Register from "./components/Register";
 import Form from "./components/Form";
 import Results from "./pages/Results";
+import Home from "./pages/Home";
+import SelfAssessment from "./pages/SelfAssessment";
 
 function App() {
   return (
     <Router>
       <Routes>
+        {/* Rotas existentes */}
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/form" element={<Form />} />
         <Route path="/results" element={<Results />} />
+        {/* Novas rotas */}
+        <Route path="/home" element={<Home />} />
+        <Route path="/self-assessment" element={<SelfAssessment />} />
       </Routes>
     </Router>
   );
