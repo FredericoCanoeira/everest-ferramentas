854107cd19fe58675ea289a1df5674c4d1e27dc0
Frederico Canoeira <fcanoeira@gmail.com>
2025-01-16 00:28:03 -0500

    Fix .Css spaces

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import './Home.css';
 
 const Home = () => {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px", }}>
+    <div className="home-container">
       <h1>Auto-avaliação de Liderança</h1>
       <p>Seja bem-vindo!</p>
-      <Link to="/assessment">
+      <Link to="/assessment" >
       {/* <button>Iniciar Autoavaliação</button> */}
       </Link>
     </div>
