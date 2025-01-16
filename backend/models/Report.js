ca8ef0d06927b4ccd3a0e6afa1b20520a74631b8
Frederico Canoeira <fcanoeira@gmail.com>
2025-01-16 10:47:32 -0500

    implement reports

---
+++ backend/models/Report.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const reportSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', // Assumindo que você tem um modelo de usuário
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Report = mongoose.model('Report', reportSchema);
+
+module.exports = Report;
--- backend/server.js
+++ backend/server.js
@@ -1,54 +1,3 @@
-/*const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
-require("dotenv").config();
-
-const app = express();
-
-// Middleware
-app.use(express.json());
-app.use(cors());
-
-// MongoDB Connection
-mongoose
-  .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.error(err));
-
-  // Import Routes
-const usersRoutes = require("./routes/users");
-const selfAssessmentRoutes = require("./routes/selfAssessment");
-
-// Routes
-app.get("/", (req, res) => res.send("Backend is running..."));
-app.use("/api/users", usersRoutes);
-app.use("/api/self-assessment", selfAssessmentRoutes);
-
-// Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));*/
-
-/*--------require('dotenv').config(); // Para carregar variáveis de ambiente
-const mongoose = require('mongoose');
-const express = require('express');
-
-const app = express();
-
-
-
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Conexão com MongoDB bem-sucedida'))
-    .catch((err) => {
-        console.error('Erro ao conectar ao MongoDB:', err.message);
-        process.exit(1); // Finaliza o processo em caso de falha
-    });
-
-// Configuração básica do servidor
-app.get('/', (req, res) => res.send('Servidor funcionando'));
-
-app.listen(5000, () => console.log('Servidor rodando na porta 5000'));----*/
-
-
 const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
@@ -75,6 +24,10 @@ app.get('/', (req, res) => res.send('Server is running'));
 // Importing user routes
 const usersRoutes = require('./routes/users');
 app.use('/api/users', usersRoutes); // Prefix routes with `/api/users`
+
+// Importing report routes
+const reportRoutes = require('./routes/reportRoutes');
+app.use('/api/reports', reportRoutes); // Prefix routes with `/api/reports`
 
 // Starting the server
 const PORT = process.env.PORT || 5000;
